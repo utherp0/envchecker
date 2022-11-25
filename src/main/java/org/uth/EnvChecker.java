a56b6725380346585971b186ace882ab60f7c55c
utherp0 <ian.lawson@redhat.com>
2022-11-25 12:44:47 +0000

    Added search

--- src/main/java/org/uth/EnvChecker.java
+++ src/main/java/org/uth/EnvChecker.java
@@ -33,6 +33,40 @@ public class EnvChecker
     return ( output == null ? "No envs found" : output.toString() );
   }
 
+  @Path("/search")
+  @GET
+  @Produces("text/html")
+  public String performSearch(@QueryParam("terms") String terms )
+  {
+    Map<String,String> allEnvs = System.getenv();
+
+    StringBuilder output = null;
+
+    long start = System.currentTimeMillis();
+
+    for( String key : allEnvs.keySet() )
+    {
+      // Match?
+      if( key.toLowerCase().indexOf( terms.toLowerCase() ) != -1 )
+      {
+        String value = System.getenv(key);
+
+        if( output == null )
+        {
+          output = new StringBuilder( "<b>" + key + ":</b> " + value );
+        }
+        else
+        {
+          output.append( "<br/><b>" + key + ":</b> " + value );
+        }
+      }
+    }
+
+    long end = System.currentTimeMillis();
+
+    return "Search for <b>" + terms + "</b> took <b>" + ( end - start ) + "ms</b><br/>" + ( output == null ? "Found 0 results." : "<br/>" + output.toString());
+  }
+
   @Path("/getenv")
   @GET
   @Produces("text/html")
